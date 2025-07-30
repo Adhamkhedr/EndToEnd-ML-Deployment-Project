5c91ce6248a15d7f48dc3e24feb042e5643a3e59
Adham Khedr <Adhamkhedr@aucegypt.edu>
2025-07-30 15:18:02 +0300

    Changes to data_transformation.py: Added a total_score column which I can use to calculate average_score that will be used as the target column instead of math_score to capture overall performance acrross multiple grades. Added a score_diff feature used to show consistency across the grades and applied threshold to these such that each range of numbers has a different label

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -9,80 +9,120 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from src.utils.logging import get_logger
 
+#same idea as ingestion.py : DataTransformationConfig to store the file path preprocessor_obj_file_path, 
+#where Python object preprocessor (which knows how to clean and transform any data) will be saved as a .pkl file.
+
 @dataclass
 class DataTransformationConfig:
-preprocessor_obj_file_path: str = os.path.join("artifacts", "preprocessor.pkl")
+    preprocessor_obj_file_path: str = os.path.join("artifacts", "preprocessor.pkl")
 
-class DataTransformation:
-    def __init__(self):
-        self.config = DataTransformationConfig()  # Load path to save preprocessor.pkl
+class DataTransformation:  #contains all the logic for transforming the data.
+    def __init__(self):      #Setup Config and Logger
+        self.config = DataTransformationConfig()  # have access to the preprocessor_obj_file_path
         self.logger = get_logger(__name__)        # Set up logging
 
-def get_preprocessor(self, numeric_columns, categorical_columns):   #function gets called in initiate_data_transformation
-    """
-    Build a ColumnTransformer with pipelines for numeric and categorical features.
-    """
-    # Pipeline for numeric features
-    numeric_pipeline = Pipeline(steps=[
-        ("imputer", SimpleImputer(strategy="median")),
-        ("scaler", StandardScaler())
-    ])
-    # Pipeline for categorical features
-    categorical_pipeline = Pipeline(steps=[
-        ("imputer", SimpleImputer(strategy="most_frequent")),
-        ("encoder", OneHotEncoder(handle_unknown="ignore")),
-        ("scaler", StandardScaler(with_mean=False))
-    ])
-    # Combine both pipelines into one transformer
-    preprocessor = ColumnTransformer(transformers=[
-        ("num", numeric_pipeline, numeric_columns),    #numeric_columns will be defined in initiate_data_transformation function
-        ("cat", categorical_pipeline, categorical_columns)   #categorical_columns will be defined in initiate_data_transformation function
-    ])
+    def get_preprocessor(self, numeric_columns, categorical_columns):   #function gets called in initiate_data_transformation
+        """
+        Builds a ColumnTransformer with pipelines for numeric and categorical features.
+        """
+        # Pipeline for numeric features
+        numeric_pipeline = Pipeline(steps=[
+            ("fill_missing", SimpleImputer(strategy="median")),
+            ("scale_numeric", StandardScaler())
+        ])
+        # Pipeline for categorical features
+        categorical_pipeline = Pipeline(steps=[
+            ("fill_missing", SimpleImputer(strategy="most_frequent")),
+            ("encoder", OneHotEncoder(handle_unknown="ignore")),
+            ("scale_cat", StandardScaler(with_mean=False))  #dont subtract the mean since they are 
+            #encoded so a 0 value may crash everything 
+        ])
+        # Combine both pipelines into one transformer
+        preprocessor = ColumnTransformer(transformers=[
+            ("num", numeric_pipeline, numeric_columns),    #numeric_columns will be defined in initiate_data_transformation function
+            ("cat", categorical_pipeline, categorical_columns)   #categorical_columns will be defined in initiate_data_transformation function
+        ])
+
+        return preprocessor
 
 
+#initiate_data_transformation should Transform training and testing data using preprocessing pipeline.
+# and Return transformed arrays and path to saved preprocessor object.
+    def initiate_data_transformation(self, train_path, test_path):
 
-def initiate_data_transformation(self, train_path, test_path):
-    """
-    Transforms training and testing data using preprocessing pipeline.
-    Returns transformed arrays and path to saved preprocessor object.
-    """
+        # Load datasets
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
 
-    # Load datasets
-    train_df = pd.read_csv(train_path)
-    test_df = pd.read_csv(test_path)
+        # Add total and average score and use average as target instead of the 3 scores 
+        for df in [train_df, test_df]:
+            df["total_score"] = df["math_score"] + df["reading_score"] + df["writing_score"]
+            df["average_score"] = df["total_score"] / 3
 
-    # Set your actual target column here
-    target_column = "target"
+        # Add score_diff feature to test consistency amon student grades
+        for df in [train_df, test_df]:
+            df["score_diff"] = df[["math_score", "reading_score", "writing_score"]].max(axis=1) - \
+                            df[["math_score", "reading_score", "writing_score"]].min(axis=1)
 
-    # Separate input features and target
-    X_train = train_df.drop(columns=[target_column])
-    y_train = train_df[target_column]
-    X_test = test_df.drop(columns=[target_column])
-    y_test = test_df[target_column]
+        # Define label functions corrsoponding to target average score and difference in score feature 
+        def get_perf_label(avg):   #shows Real gaps in performance
+            if avg >= 85:
+                return "High"
+            elif avg >= 70:
+                return "Medium"
+            else:
+                return "Low"
 
- 
-   numeric_columns = []  
-   categorical_columns = []
+        def get_consistency_label(diff): 
+            if diff <= 5:
+                return "Very Consistent"
+            elif diff <= 15:
+                return "Moderately Consistent"
+            else:
+                return "Inconsistent"
 
-    for i in X_train.columns:
-        if X_train[i].dtype in ["int64", "float64"]:
-            numeric_columns.append(i)
-        elif X_train[i].dtype == "object":
-            categorical_columns.append(i)
+        # Apply labels
+        for df in [train_df, test_df]:
+            df["performance_label"] = df["average_score"].apply(get_perf_label)
+            df["consistency_label"] = df["score_diff"].apply(get_consistency_label)
+            df["summary_label"] = df["performance_label"] + " — " + df["consistency_label"]
 
-    # Build preprocessor
-    preprocessor = self.get_preprocessor(numeric_columns, categorical_columns)
+        # Now set target
+        target_column = "average_score"
 
-    # Apply transformation
-    X_train_transformed = preprocessor.fit_transform(X_train)
-    X_test_transformed = preprocessor.transform(X_test)
+        # Separate input features and target
+        X_train = train_df.drop(columns=[target_column])
+        y_train = train_df[target_column]
+        X_test = test_df.drop(columns=[target_column])
+        y_test = test_df[target_column]
 
-    # Save the fitted preprocessor object
-    with open(self.config.preprocessor_obj_file_path, "wb") as f:
-        pickle.dump(preprocessor, f)
+        # Determine numeric and categorical columns
+        numeric_columns = []
+        categorical_columns = []
+#add numeric features to numeric_columns and categorical features to categorical_columns
+        for col in X_train.columns:
+            if X_train[col].dtype in ["int64", "float64"]:
+                numeric_columns.append(col)
+            elif X_train[col].dtype == "object":
+                categorical_columns.append(col)
 
-    # Combine transformed features with targets
-    train_arr = np.c_[X_train_transformed, y_train]
-    test_arr = np.c_[X_test_transformed, y_test]
 
-    return train_arr, test_arr, self.config.preprocessor_obj_file_path
+        if "summary_label" in categorical_columns:
+            categorical_columns.remove("summary_label")
+
+        # Build preprocessor
+        preprocessor = self.get_preprocessor(numeric_columns, categorical_columns)
+
+        # Apply transformation
+        X_train_transformed = preprocessor.fit_transform(X_train)
+        X_test_transformed = preprocessor.transform(X_test)
+
+        # Save the fitted preprocessor object
+        with open(self.config.preprocessor_obj_file_path, "wb") as f:
+            pickle.dump(preprocessor, f)
+
+        # Combine transformed features with targets to stack arrays side by side
+        train_arr = np.c_[X_train_transformed, y_train]
+        test_arr = np.c_[X_test_transformed, y_test]
+
+        return train_arr, test_arr, self.config.preprocessor_obj_file_path
